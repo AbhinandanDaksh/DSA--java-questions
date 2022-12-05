d46e11981552a4451722c01549d4a312b999857d
AbhinandanDaksh <117848859+AbhinandanDaksh@users.noreply.github.com>
2022-12-05 21:05:18 +0530

    Add files via upload

---
+++ BracketBack.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BracketBack {
+    static List<String> generate(int n){
+        List<String> output= new ArrayList<>();
+        helaper(output, "", 0, 0, n);
+        return output;
+
+    }
+    static List<String> helaper(List<String> ans,String currebracket,int open,int close,int max){
+        if(currebracket.length()==max*2){
+            ans.add(currebracket);
+            return ans;
+        }
+        if(open<max){
+            helaper(ans, currebracket+"(", open+1, close, max);
+        }
+        if(close<open){
+            helaper(ans, currebracket+")", open, close+1, max);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(generate(3));
+    }
+    
+}
