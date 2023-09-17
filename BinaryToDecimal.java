faec0b1f3a3a54cb2ccbc5b6880ec8f1c26387ba
Abhinandan <117848859+AbhinandanDaksh@users.noreply.github.com>
2023-09-17 16:17:11 +0530

    Update BinaryToDecimal.java

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -15,11 +15,26 @@ public class BinaryToDecimal {
         }
         System.out.println(dec);
     }
+    public static void DecimalToBinary(int n) {
+        int bin=0;
+        int pow=0;
+        while(n>0){
+            int rem=n%2;
+            bin=bin+(rem*(int)(Math.pow(10, pow)));
+            pow++;
+            n=n/2;
+        }
+        System.out.println(bin);
+        
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out,println("Enter binary numbers");
         int n=sc.nextInt();
-        // PrimeRange(n);
         binaryToDecimal(n);
+        System.out,println("Enter Decimal numbers");
+        int m=sc.nextInt();
+        DecimalToBinary(m);
 
 
         
