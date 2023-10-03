47065f8ce219490b569907add35a1fd467e4b402
Abhinandan <117848859+AbhinandanDaksh@users.noreply.github.com>
2023-10-03 15:41:35 +0530

    Create Problam238.java

---
+++ Problam238.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int res[]=new int[n];
+       
+        for(int i=0;i<n;i++){
+            int mul=1;
+            for(int j=0;j<n;j++){
+                if(i==j){continue;}
+                mul*=nums[j];
+            }
+            res[i]=mul;
+        }
+        return res;
+    }
+}
