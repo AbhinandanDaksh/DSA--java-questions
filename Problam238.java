5c5293a83f7a9936d24e8aa9e3198c33d855b078
Abhinandan <117848859+AbhinandanDaksh@users.noreply.github.com>
2023-10-04 15:02:02 +0530

    Update Problam238.java

--- Problam238.java
+++ Problam238.java
@@ -2,14 +2,24 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
         int res[]=new int[n];
-       
-        for(int i=0;i<n;i++){
-            int mul=1;
-            for(int j=0;j<n;j++){
-                if(i==j){continue;}
-                mul*=nums[j];
-            }
-            res[i]=mul;
+        int prefix[]=new int[n];
+        int postfix[] =new int[n];
+
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]*nums[i];
+        }
+
+        postfix[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            postfix[i]=postfix[i+1]*nums[i];
+        }
+
+        res[0]=postfix[1];
+        res[n-1]=prefix[n-2];
+
+        for(int i=1;i<n-1;i++){
+            res[i]=prefix[i-1]*postfix[i+1];
         }
         return res;
     }
