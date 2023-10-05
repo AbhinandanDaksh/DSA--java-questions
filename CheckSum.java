bbc5e49b4ab0e1807b29dde2029bc15c07373984
Abhinandan <117848859+AbhinandanDaksh@users.noreply.github.com>
2023-10-05 11:52:19 +0530

    Create CheckSum.java

---
+++ CheckSum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Day23 {
+
+    public static boolean checksum(int arr[],int k){
+        Arrays.sort(arr);
+        int n=arr.length;
+        int start=0;
+        int end=n-1;
+        for(int i=0;i<n;i++){
+            if(k==arr[start]+arr[end]){
+                return true;
+
+            }
+            if(k<arr[i]+arr[n-i-1]){
+                end--;
+            }
+            else{
+                start++;
+            }
+            
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[]={11, 15, 26, 38, 9, 10};
+        int target=55;
+        System.out.println(checksum(arr,target));
+    }
+    
+}
